568bc8b9d97ebfeb74e4dbe3520b8e02f09b89f1
VivasvanVerma <vvofficial.yt@gmail.com>
2025-03-29 12:37:05 +0530

    typecasting

---
+++ Typecasting.py
@@ -0,0 +1,13 @@
+name = "John"
+age = 14
+is_student = True
+height = 176.5
+age = str(age)
+print("Data type of age is: ", type(age))
+print("Age: ", age)
+height = int(height)
+print("Data type of height is: ", type(height))
+print("Height: ", height)
+name = bool(name)
+print("Data type of name: ", type(name))
+print("Name: ", name)
