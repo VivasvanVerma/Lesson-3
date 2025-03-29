3cb75ae6a84826bc919271a238e8af98c70f638a
VivasvanVerma <vvofficial.yt@gmail.com>
2025-03-29 12:50:35 +0530

    Operations

---
+++ reverse.py
@@ -0,0 +1,10 @@
+text = input("Enter a string: ")
+revtext = text[::-1]
+text = revtext
+print("Reverse of the given text is: ", text)
+x = "python"
+print(x)
+print(x.upper())
+y = "CODING"
+print(y)
+print(y.lower())
