a151544cc21d4840e15f0b1f5725f4d544eb19a5
VivasvanVerma <vvofficial.yt@gmail.com>
2025-03-29 13:00:55 +0530

    operations hw

---
+++ OperationsHW.py
@@ -0,0 +1,7 @@
+text = input("Enter a lowercase string: ")
+print(text.upper())
+text1 = input("Now enter an uppercase string: ")
+print(text1.lower())
+text2 = "Hello World!"
+print(text2.find("World"))
+print(text2.replace("World", "Python"))
